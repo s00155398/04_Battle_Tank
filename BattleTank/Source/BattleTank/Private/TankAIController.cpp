7b8cff49bd980d21fb97651c64fa440658b5a268
Admin-HP\Admin <s00155398@mail.itsligo.ie>
2017-12-17 16:01:25 +0000

    Player & AI Controller.
    Player and AI controllers implemented and attached to respective
    entities.

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankAIController.h"
+#include "Engine/World.h"
+
+
+void ATankAIController::BeginPlay()
+{
+Super::BeginPlay();
+
+auto PlayerTank = GetPlayerTank();
+if (!PlayerTank)
+{
+	UE_LOG(LogTemp, Warning, TEXT("AI Controller not find player tank"));
+}
+else
+{
+	UE_LOG(LogTemp, Warning, TEXT("AI Controller found player %s"), *(PlayerTank->GetName()));
+}
+}
+
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) {return nullptr;}
+	return Cast<ATank>(PlayerPawn);
+}
